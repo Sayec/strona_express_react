f4d2ceb63bdfb301a11ec6e565bf98201425b44f
Sayec <kamilbrzezowski@wp.pl>
2021-07-01 14:47:37 +0200

    fdsd

--- client/src/App/pages/Page.js
+++ client/src/App/pages/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Default from './Default';
@@ -12,18 +12,20 @@ import ObjectComp from './ObjectComp';
 
 const Page = () => {
   return (
-    <div className="page-container">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route exact path="/gallery" component={Gallery} />
-        <Route path="/eq" component={Eq} />
-        <Route path="/upload/:category/:object" component={Id} />
-        <Route path="/gallery/:category/:object" component={ObjectComp} />
-        <Route path="/gallery/:category" component={Category} />
-        <Route path="/" component={Default} />
-      </Switch>
-    </div>
+    <BrowserRouter>
+      <div className="page-container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route path="/eq" component={Eq} />
+          <Route path="/upload/:category/:object" component={Id} />
+          <Route path="/gallery/:category/:object" component={ObjectComp} />
+          <Route path="/gallery/:category" component={Category} />
+          <Route path="/" component={Default} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
