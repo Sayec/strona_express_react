20b20d501d181ee111a2e5f642473553a6cf8371
Sayec <kamilbrzezowski@wp.pl>
2021-08-16 00:40:35 +0200

    cganhe

--- api/index.js
+++ api/index.js
@@ -46,13 +46,13 @@ app.use(
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 } else {
-  // app.use(express.static(path.join(__dirname, '/client/public')));
-  // app.get('/*', function (req, res) {
-  //   res.sendFile(path.join(__dirname, './client/public/index.html'));
-  // });
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/public/index.html'));
+  });
 }
 
 listRoutes(app);
