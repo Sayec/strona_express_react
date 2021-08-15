073a77ac3bc87f910b323f5615b74e3d1450bb47
Sayec <kamilbrzezowski@wp.pl>
2021-08-16 01:10:47 +0200

    React Route works on prod

--- api/index.js
+++ api/index.js
@@ -39,32 +39,12 @@ app.use(
     maxAge: config.maxAgeSession, // 24 hours
   })
 );
-// app.use(function (req, res, next) {
-//   res.setHeader('Content-Type', 'application/json');
-//   next();
-// });
 
 listRoutes(app);
 galleryRoutes(app, path, db);
 homeRoutes(app, path);
 handleFormPost(app, path, fs, db);
 
-// An api endpoint that returns a short list of items
-// app.get('/api/getList', (req, res) => {
-//   const list = ['item1', 'item2', 'item3', 'dsadsa', 'test123'];
-//   console.log(list);
-//   res.json(list);
-//   console.log('Sent list of items');
-// });
-
-// Handles any requests that don't match the ones above
-// app.get('*', (req, res) => {
-//   console.log('test');
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-// app.get('*', (req, res) =>
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// );
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('/*', function (req, res) {
