fa7d07f98e69246624cb5a3f68fa58cdfd007aee
Sayec <kamilbrzezowski@wp.pl>
2021-08-16 01:12:17 +0200

    React Route works on prod

--- api/index.js
+++ api/index.js
@@ -39,38 +39,12 @@ app.use(
     maxAge: config.maxAgeSession, // 24 hours
   })
 );
-<<<<<<< HEAD
-=======
-// app.use(function (req, res, next) {
-//   res.setHeader('Content-Type', 'application/json');
-//   next();
-// });
->>>>>>> 5c3166660951cd202793f91455c5e6baa4ee3bc4
 
 listRoutes(app);
 galleryRoutes(app, path, db);
 homeRoutes(app, path);
 handleFormPost(app, path, fs, db);
 
-<<<<<<< HEAD
-=======
-// An api endpoint that returns a short list of items
-// app.get('/api/getList', (req, res) => {
-//   const list = ['item1', 'item2', 'item3', 'dsadsa', 'test123'];
-//   console.log(list);
-//   res.json(list);
-//   console.log('Sent list of items');
-// });
-
-// Handles any requests that don't match the ones above
-// app.get('*', (req, res) => {
-//   console.log('test');
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-// app.get('*', (req, res) =>
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// );
->>>>>>> 5c3166660951cd202793f91455c5e6baa4ee3bc4
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('/*', function (req, res) {
