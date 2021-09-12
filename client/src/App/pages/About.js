c1fc71388268e3606eb940268f1b08ac38dfc3d9
Sayec <kamilbrzezowski@wp.pl>
2021-09-12 19:16:38 +0200

    added about section

--- client/src/App/pages/About.js
+++ client/src/App/pages/About.js
@@ -3,8 +3,18 @@ import { Link, useParams } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div>
-      O mnie coś
+    <div className="about-container">
+      <p className="text-about">
+        Nazywam się Kamil Brzezowski. Astronomią interesuję się od ponad 2 lat.
+        Pewnego razu podczas oglądanie nocnego nieba przez teleskop pojawiła się
+        u mnie chęć uwiecznienia tego co oglądam.
+        <br /> Do fotografowania nieba wykorzystuję:{' '}
+        <ul>
+          <li>Teleskop Skywatcher 150/750</li>
+          <li>Lustrzanka Canon 1100D</li>
+          <li>Statyw EQ3-2 ze stalowym statywem</li>
+        </ul>{' '}
+      </p>
       <Link to={'./'}>
         <button variant="raised">Wróć</button>
       </Link>
