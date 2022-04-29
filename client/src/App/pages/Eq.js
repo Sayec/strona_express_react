741462987bf63cd0b938d4e8bbfe3403fe7c6355
Sayec <56696517+Sayec@users.noreply.github.com>
2022-04-30 00:25:29 +0200

    Update Eq.js

--- client/src/App/pages/Eq.js
+++ client/src/App/pages/Eq.js
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 const Eq = () => {
   return (
     <div>
-      O eq coś
+      O 
       <Link to={'./'}>
         <button variant="raised">Home</button>
       </Link>
